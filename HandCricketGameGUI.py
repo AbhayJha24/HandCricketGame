4a91597f52742386071fdcf1c3168ed9663005c1
Abhay <abhay.jha243@gmail.com>
2021-10-27 17:00:13 +0530

    Font Styling changed in GUI version

--- HandCricketGameGUI.py
+++ HandCricketGameGUI.py
@@ -13,15 +13,15 @@ root.minsize(640,480)
 root.title("Hand Cricket Game")
 
 # Label
-h1 = tkinter.Label(text="Hello World")
-h2 = tkinter.Label(text="Next Line")
+h1 = tkinter.Label(text="Hand Cricket Game", font=("Ink Free", 20, "bold"))
+#h2 = tkinter.Label(text="Next Line")
 h1.pack()
-h2.pack()
+#h2.pack()
 
 # Images
 
 bat = tkinter.PhotoImage(file="bat.png")
-image = tkinter.Label(image=bat)
+image = tkinter.Label(image= bat)
 image.pack()
 
 # This mainloop should remain at the end
